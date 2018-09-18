20b46275827f0ba49b8e20033a7cb180d1fe4199
CeliaEva98 <celia.eva12345@gmail.com>
2018-09-18 18:12:32 +1200

    Another commit

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -4,6 +4,7 @@ public class PartD {
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Celia!");
 		System.out.println("My favourite colour is  yellow");
+		System.out.println("My favourite mode of transportation is none");
 	}
 
 }
